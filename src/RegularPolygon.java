e1b7a78be73f0f44f58836809dab4595473a0733
nemanjagajic <Nemanja.gajicru96@gmail.com>
2017-05-17 14:10:57 +0200

    polygon added

--- src/RegularPolygon.java
+++ src/RegularPolygon.java
@@ -1,8 +1,33 @@
+import javafx.scene.paint.Color;
 import mars.drawingx.drawing.Drawing;
+import mars.drawingx.drawing.DrawingUtils;
+import mars.drawingx.drawing.View;
+import mars.drawingx.gadgets.annotations.GadgetDouble;
+import mars.geometry.Vector;
 
-public class RegularPolygon {
+public class RegularPolygon implements Drawing {
 
-    public static void main(String[] args) {
-        Drawing.launch(700, 700);
+    Integer n = 5;
+
+    @GadgetDouble(min = 0, max = 300)
+    Double r = 200.0;
+
+    @GadgetDouble(min = 0, max = 2 * Math.PI)
+    Double alpha = 0.0;
+
+    @Override
+    public void draw(View view) {
+        DrawingUtils.clear(view, Color.hsb(0, 0, 0.2));
+        view.setStroke(Color.GRAY);
+        view.setLineWidth(2.0);
+
+        for (int i = 0; i < n; i++) {
+            view.strokeLine(getVertex(i), getVertex(i + 1));
+        }
     }
+
+    Vector getVertex(int i) { return Vector.polar(r, alpha + i * 2 * Math.PI / n); }
+
+    public static void main(String[] args) { Drawing.launch(500, 500); }
+
 }
